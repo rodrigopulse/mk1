8fcc365eca75e6f88c4763e99b4914acb4b3fa09
rodrigopulse <contato@rodrigogodoy.net>
2019-11-30 18:03:46 -0300

    array com as imagens

--- src/componentes/GaleriaFotos/GaleriaFotos.js
+++ src/componentes/GaleriaFotos/GaleriaFotos.js
@@ -8,10 +8,26 @@ import imagem1 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-1.jpg';
 
 class GaleriaFotos extends React.Component {
   state = { isOpen: false}
+  imagens = [
+    {
+      imagem: imagem1,
+      thumb: thumb1
+    },
+    {
+      imagem: imagem1,
+      thumb: thumb1
+    },
+    {
+      imagem: imagem1,
+      thumb: thumb1
+    }
+  ]
   render() {
+    console.log(this.imagens);
     const { isOpen } = this.state;
     return(
       <div className="container galeria-fotos">
+
         {isOpen && (
           <div
             className="galeria-modal"
@@ -30,28 +46,24 @@ class GaleriaFotos extends React.Component {
             </div>
           </div>
         )}
-        <div className="galeria-fotos__thumb">
-          <button className="galeria-fotos__botao"
-            onClick={
-              () => this.setState({
-                isOpen: true,
-                imagemShow: imagem1
-              })
-            }>
-            <img src={thumb1} alt="Crossfit Mk1" />
-          </button>
-        </div>
-        <div className="galeria-fotos__thumb">
-          <button className="galeria-fotos__botao">
-            <img src={thumb1} alt="Crossfit Mk1" />
-          </button>
-        </div>
-        <div className="galeria-fotos__thumb">
-          <button className="galeria-fotos__botao">
-            <img src={thumb1} alt="Crossfit Mk1" />
-          </button>
+
+        <div className="galeria-fotos">
+          {this.imagens.map((imagem)=> (
+            <div className="galeria-fotos__thumb">
+              <button className="galeria-fotos__botao"
+              onClick={
+                () => this.setState({
+                  isOpen: true,
+                  imagemShow: imagem.imagem
+                })
+              }>
+              <img src={imagem.thumb} alt="Crossfit Mk1" />
+            </button>
+          </div>
+          ))}
         </div>
       </div>
+
     );
   }
 
