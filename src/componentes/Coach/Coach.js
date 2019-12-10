4b75256d988bc3045a4ce86b6331e03e3af1642f
Rodrigo Godoy <contato@rodrigogodoy.net>
2019-12-09 21:08:36 -0300

    colocado o conteúdo do head coach

--- src/componentes/Coach/Coach.js
+++ src/componentes/Coach/Coach.js
@@ -15,7 +15,22 @@ class Coach extends React.Component {
         <div className="container container--max">
           <div className="coach__conteudo">
             <h3 className="txt-h1">Nosso head coach</h3>
-            <p>Lorem ipsum</p>
+            <h4 className="coach__nome">Marcel Ligotti</h4>
+            <p>Graduado em Educação Física e Pós-graduado em Condicionamento Físico e Administração e Marketing Esportivo, possui mais de 24 anos de experiência na área de treinamento desportivo e saúde.</p>
+            <p>Foi responsável técnico de grandes academias em São Paulo com experiência em cursos de fisiologia do exercício além de ser um experiente atleta de CrossFit, Karatê Kyokushin Oyama e Kick Boxe.</p>
+            <ul>
+              <li>• Comentarista da ESPN – Crossfit Games 2014, 2015 e 2016</li>
+              <li>• Comentarista CrossFit BCC – CrossFit games 2019</li>
+              <li>• Comentarista CrossTV • CrossFit Level 1</li>
+              <li>• CrossFit Level 2</li>
+              <li>• Crossfit Weightlifting Trainer Course</li>
+              <li>• Capacitação Crossfit Brasil Nível I e II</li>
+              <li>• Certificação em Levantamento de Peso Olímpico – Fortify</li>
+              <li>• Certificação em movimentos ginásticos com Marcos Goto (técnico do Arthur Zanetti)</li>
+              <li>• CrossFit Endurance Trainer</li>
+              <li>• Curso MAD Powerlifting</li>
+              <li>• F.Reis Weightlifting Level 1</li>
+            </ul>
           </div>
         </div>
       </div>
