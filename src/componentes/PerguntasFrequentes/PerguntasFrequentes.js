a7d4b38dde1f6be113e499db6d445caf854fa126
Rodrigo Godoy <contato@rodrigogodoy.net>
2019-12-30 17:06:12 -0300

    colocadas as perguntas frequentes

--- src/componentes/PerguntasFrequentes/PerguntasFrequentes.js
+++ src/componentes/PerguntasFrequentes/PerguntasFrequentes.js
@@ -16,18 +16,43 @@ class PerguntasFrequentes extends React.Component {
           <Accordion>
             <Card>
               <Accordion.Toggle as={Card.Header} eventKey="0">
-                <span>Crossfit machuca?</span> <img src={Seta} alt="Seta" />
+                <span>Estou acima do peso... posso treinar CrossFit?</span> <img src={Seta} alt="Seta" />
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
-                <Card.Body>Aqui vai o texto que abre</Card.Body>
+                <Card.Body>Sim, vc pode!<br />Na verdade o CrossFit é uma das atividades mais indicadas para perda peso, ganho de massa muscular e saúde, devido a sua metodologia de treino e o alto gasto calórico</Card.Body>
               </Accordion.Collapse>
             </Card>
             <Card>
               <Accordion.Toggle as={Card.Header} eventKey="1">
-                <span>Tenho uma lesão, posso treinar mesmo assim?</span> <img src={Seta} alt="Seta" />
+                <span>Qualquer pessoa pode fazer CrossFit?</span> <img src={Seta} alt="Seta" />
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="1">
-                <Card.Body>Aqui vai o texto que abre</Card.Body>
+                <Card.Body>O CrossFit é uma atividade física que exige que você seja bem orientado para que todo o seu treino seja adaptado ao seu nível de condicionamento físico e necessidade. Para que isto ocorra, você será orientado por profissionais formados em educação física e grande experiência com a atividade.</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="2">
+                <span>É verdade que a mulher fica musculosa treinando CrossFit?</span> <img src={Seta} alt="Seta" />
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="2">
+                <Card.Body>Se esse for o objetivo dela, ela deverá ser orientada por um médico e um nutricionista, para que a sua alimentação proporcione o grau de hipertrofia desejado, caso contrário você irá conquistar um corpo mais "atlético" e com certeza mais saudável.</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="3">
+                <span>Criança pode treinar CrossFit?</span> <img src={Seta} alt="Seta" />
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="3">
+                <Card.Body>Ela deve treinar CrossFit! O CrossFit por ser um esporte que engloba outros esportes, faz com que a criança na sua fase de crescimento, adquira uma consciência corporal e formação estrutural diferenciada em relação a outros esportes.</Card.Body>
+              </Accordion.Collapse>
+            </Card>
+            <Card>
+              <Accordion.Toggle as={Card.Header} eventKey="4">
+                <span>Para dar aula de CrossFit, basta ter o Level I?</span> <img src={Seta} alt="Seta" />
+              </Accordion.Toggle>
+              <Accordion.Collapse eventKey="4">
+                <Card.Body>Todos os profissionais que trabalham com CrossFit, devem ser formados em Educação Física.<br />
+Nossa sugestão é que você consulte o CREF (Conselho Regional de Educação Física) e verifique se o profissional que irá lhe atender seja credenciado.</Card.Body>
               </Accordion.Collapse>
             </Card>
           </Accordion>
