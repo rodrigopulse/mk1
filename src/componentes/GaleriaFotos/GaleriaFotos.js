2249e1fb07f69d1b249e9f4c12f7956d39f91c3b
Rodrigo Godoy <contato@rodrigogodoy.net>
2021-09-30 09:47:43 -0300

    inserida as fotos da unidade 2

--- src/componentes/GaleriaFotos/GaleriaFotos.js
+++ src/componentes/GaleriaFotos/GaleriaFotos.js
@@ -1,92 +1,100 @@
-import React from 'react';
+import React from "react";
 // sass
-import "./GaleriaFotos.scss"
+import "./GaleriaFotos.scss";
 
-import Fechar from '../../assets/imagens/icones/fechar.svg';
-import thumb1 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-1-thumb.jpg';
-import imagem1 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-1.jpg';
-import thumb2 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-2-thumb.jpg';
-import imagem2 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-2.jpg';
-import thumb3 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-3-thumb.jpg';
-import imagem3 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-3.jpg';
-import thumb4 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-4-thumb.jpg';
-import imagem4 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-4.jpg';
-import thumb5 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-5-thumb.jpg';
-import imagem5 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-5.jpg';
-import thumb6 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-6-thumb.jpg';
-import imagem6 from '../../assets/imagens/espaco/crossfit-guarulhos-mk1-6.jpg';
+import Fechar from "../../assets/imagens/icones/fechar.svg";
+import thumb1 from "../../assets/imagens/espaco/crossfit-guarulhos-mk1-1-thumb.jpg";
+import imagem1 from "../../assets/imagens/espaco/crossfit-guarulhos-mk1-1.jpg";
+import thumb2 from "../../assets/imagens/espaco/crossfit-guarulhos-mk1-2-thumb.jpg";
+import imagem2 from "../../assets/imagens/espaco/crossfit-guarulhos-mk1-2.jpg";
+import thumb3 from "../../assets/imagens/espaco/crossfit-guarulhos-mk1-3-thumb.jpg";
+import imagem3 from "../../assets/imagens/espaco/crossfit-guarulhos-mk1-3.jpg";
+import thumb4 from "../../assets/imagens/espaco/unidade-2-thumb.jpg";
+import imagem4 from "../../assets/imagens/espaco/unidade-2.jpg";
+import thumb5 from "../../assets/imagens/espaco/unidade-2-1-thumb.jpg";
+import imagem5 from "../../assets/imagens/espaco/unidade-2-1.jpg";
+import thumb6 from "../../assets/imagens/espaco/unidade-2-2-thumb.jpg";
+import imagem6 from "../../assets/imagens/espaco/unidade-2-2.jpg";
 
 class GaleriaFotos extends React.Component {
-  state = { isOpen: false}
+  state = { isOpen: false, imagemShow: "", tituloShow: "" };
   imagens = [
     {
       imagem: imagem1,
-      thumb: thumb1
+      thumb: thumb1,
+      titulo: "Unidade 1",
     },
     {
       imagem: imagem2,
-      thumb: thumb2
+      thumb: thumb2,
+      titulo: "Unidade 1",
     },
     {
       imagem: imagem3,
-      thumb: thumb3
+      thumb: thumb3,
+      titulo: "Unidade 1",
     },
     {
       imagem: imagem4,
-      thumb: thumb4
+      thumb: thumb4,
+      titulo: "Unidade 2",
     },
     {
       imagem: imagem5,
-      thumb: thumb5
+      thumb: thumb5,
+      titulo: "Unidade 2",
     },
     {
       imagem: imagem6,
-      thumb: thumb6
-    }
-  ]
+      thumb: thumb6,
+      titulo: "Unidade 2",
+    },
+  ];
   render() {
     const { isOpen } = this.state;
-    return(
+    return (
       <div className="container galeria-fotos">
-
         {isOpen && (
           <div
             className="galeria-modal"
-            onClick={
-            () => this.setState({
-              isOpen: false,
-              imagemShow: ''
-            })
+            onClick={() =>
+              this.setState({
+                isOpen: false,
+                imagemShow: "",
+                tituloShow: "",
+              })
             }
-           >
-             <button className="galeria-modal__fechar">
-               <img src={Fechar} alt="Fechar Modal" />
-             </button>
+          >
+            <button className="galeria-modal__fechar">
+              <img src={Fechar} alt="Fechar Modal" />
+            </button>
             <div className="galeria-modal__imagem">
               <img src={this.state.imagemShow} alt="Crossfit Mk1" />
             </div>
+            <p className="galeria-modal__titulo">{this.state.tituloShow}</p>
           </div>
         )}
 
         <div className="galeria-fotos">
-          {this.imagens.map((imagem, index)=> (
-            <div key = { index } className="galeria-fotos__thumb">
-              <button className="galeria-fotos__botao"
-              onClick={
-                () => this.setState({
-                  isOpen: true,
-                  imagemShow: imagem.imagem
-                })
-              }>
-              <img src={imagem.thumb} alt="Crossfit Mk1" />
-            </button>
-          </div>
+          {this.imagens.map((imagem, index) => (
+            <div key={index} className="galeria-fotos__thumb">
+              <button
+                className="galeria-fotos__botao"
+                onClick={() =>
+                  this.setState({
+                    isOpen: true,
+                    imagemShow: imagem.imagem,
+                    tituloShow: imagem.titulo,
+                  })
+                }
+              >
+                <img src={imagem.thumb} alt="Crossfit Mk1" />
+              </button>
+            </div>
           ))}
         </div>
       </div>
-
     );
   }
-
 }
 export default GaleriaFotos;
--- src/core/Footer/Footer.js
+++ src/core/Footer/Footer.js
@@ -1,35 +1,33 @@
-import React from 'react';
+import React from "react";
 //sass
-import './Footer.scss';
+import "./Footer.scss";
 
 //imagens sociais
-import iconeInstagram from '../../assets/imagens/social/instagram.svg';
-import iconeFacebook from '../../assets/imagens/social/facebook.svg';
+import iconeInstagram from "../../assets/imagens/social/instagram.svg";
+import iconeFacebook from "../../assets/imagens/social/facebook.svg";
 //import iconeYoutube from '../../assets/imagens/social/youtube.svg';
 
 class Footer extends React.Component {
   render() {
-    return(
-      <div className='container'>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d640.7250372690984!2d-46.56286085462839!3d-23.46021705030715!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef5a48d8d4337%3A0x9f34eee37cc8221e!2sAv.%20Em%C3%ADlio%20Ribas%2C%203243%20-%20Jardim%20Vila%20Galvao%2C%20Guarulhos%20-%20SP!5e0!3m2!1spt-BR!2sbr!4v1592758705766!5m2!1spt-BR!2sbr" width="100%" height="350" aria-hidden="false"></iframe>
-        <footer className="footer">
-          <div className="container--max">
-            <span className="footer__telefone">11 93022-9182</span>
-            <p className="footer__endereco">Av Emílio Ribas, nº 3243 <br />
-            (esquina com a Rua José Bonifácio) - Guarulhos / SP</p>
-          </div>
-          <div className="footer__sociais">
-            <a target="_blank" href="https://www.instagram.com/crossfitmk1/">
-              <img src={ iconeInstagram } alt="Instagram"/>
-            </a>
-            <a target="_blank" href="https://www.facebook.com/crossfitmk1/">
-              <img src={ iconeFacebook } alt="Facebook"/>
-            </a>
-          </div>
-        </footer>
-      </div>
+    return (
+      <footer className="footer">
+        <div className="container--max">
+          <span className="footer__telefone">11 93022-9182</span>
+          <p className="footer__endereco">
+            Av Emílio Ribas, nº 3243 <br />
+            (esquina com a Rua José Bonifácio) - Guarulhos / SP
+          </p>
+        </div>
+        <div className="footer__sociais">
+          <a target="_blank" href="https://www.instagram.com/crossfitmk1/">
+            <img src={iconeInstagram} alt="Instagram" />
+          </a>
+          <a target="_blank" href="https://www.facebook.com/crossfitmk1/">
+            <img src={iconeFacebook} alt="Facebook" />
+          </a>
+        </div>
+      </footer>
     );
   }
-
 }
 export default Footer;
